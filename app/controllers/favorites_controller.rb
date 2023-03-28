91bf0f208f6f3f897f28d48e08eb76a910aca508
cherifb16 <cherifbobbyray@gmail.com>
2023-03-28 13:47:11 +0200

    implementing asynchronous favorite function with ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,17 @@
 class FavoritesController < ApplicationController
+  before_action :set_blog
 
   def create
     current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
   end
 
   def destroy
     current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
   end
 
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
+  end
 end
